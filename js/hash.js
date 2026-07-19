280105f525e5c52702ac4a714cd91c73cf4c8126
templeguidegujarat01 <baba07yaga10@gmail.com>
2026-07-19 16:12:52 +0530

    Create hash.js

---
+++ js/hash.js
@@ -0,0 +1,13 @@
+async function generateSHA256(file) {
+
+    const buffer = await file.arrayBuffer();
+
+    const hashBuffer = await crypto.subtle.digest("SHA-256", buffer);
+
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    return hashArray
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
+
+}
